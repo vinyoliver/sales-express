a18e86d6ea790000b3027779905a2bc841d7d2ae
Viny Machado <vinyoliver.ti@gmail.com>
2018-08-31 22:45:25 -0400

    adding bootstrap

--- models/sale.js
+++ models/sale.js
@@ -2,6 +2,7 @@
 var mongoose = require('mongoose');
 
 var saleSchema = mongoose.Schema({
+    title: { type: String, required: true },
     description: { type: String, required: true },
     url: { type: String, required: true },
     price: { type: Number, required: true },
